e055fc9a3094e5e7ebbdb10c7e525ef61394667e
- <hugo.duda@protonmail.com>
2023-12-27 13:43:01 +0100

    [ADD] All setting_up test + 5 generatings test with optimisation

--- setting_up/test.sh
+++ setting_up/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function test () {
-    echo -e "\n\n\n\e[1;36m---------[ERROR MANAGER]---------\n\e[0m"; sleep 0.1
+    echo -e "\n\n\n\e[1;36m------------------------[ERROR HANDLER]------------------------\n\e[0m"; sleep 0.1
 
-    echo -e "\n\e[1;33m--[Robustness - Opening valid file]--\n\e[0m"; sleep 0.05
+    echo -e "\n\e[1;33m-----[Robustness - Opening valid file]-----\n\e[0m"; sleep 0.05
 
     echo -e "\e[1;37mTest n°1 (Bad character) :" ; sleep 0.05
     ./setting_up bash_test/bad_caracter_map
@@ -60,7 +60,7 @@ function test () {
     fi
     rm return.txt
 
-    echo -e "\n\n\n\e[1;33m--[Robustness - Generating valid board]--\n\e[0m"; sleep 0.1
+    echo -e "\n\e[1;33m-----[Robustness - Generating valid board]-----\n\e[0m"; sleep 0.1
 
     echo -e "\e[1;37mTest n°1 (Alpha-numeric size) :" ; sleep 0.05
     ./setting_up a ..o..o
@@ -117,7 +117,9 @@ function test () {
     fi
     rm return.txt
 
-    echo -e "\n\n\n\e[1;36m---------[ALGORITHM APP]---------\n\n\e[0m"; sleep 0.1
+    echo -e "\n\n\n\e[1;36m------------------------[ALGORITHM APP]------------------------\n\n\e[0m"; sleep 0.1
+
+    echo -e "\n\e[1;33m-----[Opening map (no timout, carful of infinite loop)]-----\n\e[0m"; sleep 0.1
 
     echo -e "\e[1;37mtest n°1 (intermediate_map_34_137_empty) :"; sleep 0.1
     ./setting_up example_files/maps/intermediate_map_34_137_empty > test_bash_log.txt
@@ -321,21 +323,23 @@ function test () {
 
     echo -e "\e[1;37mtest n°21 (intermediate_map_1000_1000) :"; sleep 0.1
     ./setting_up example_files/maps/intermediate_map_1000_1000 > test_bash_log.txt
-    sdiff -s test_bash_log.txt example_files/solved/intermediate_map_1000_1000
+    sdiff -s test_bash_log.txt example_files/solved/intermediate_map_1000_1000 > /dev/null
     if [ $? -eq 0 ]; then
         echo -e "\e[1;32mSUCESS\n\e[0m"
     else
         echo -e "\e[1;31mFAILURE\n\e[0m"
+        echo -e "\e[0;31mNote : I volontary don't print the diff for 1000x1000 map\n\e[0m"
     fi
     rm test_bash_log.txt
 
     echo -e "\e[1;37mtest n°22 (intermediate_map_1000_1000_2) :"; sleep 0.1
     ./setting_up example_files/maps/intermediate_map_1000_1000_2 > test_bash_log.txt
-    sdiff -s test_bash_log.txt example_files/solved/intermediate_map_1000_1000_2
+    sdiff -s test_bash_log.txt example_files/solved/intermediate_map_1000_1000_2 > /dev/null
     if [ $? -eq 0 ]; then
         echo -e "\e[1;32mSUCESS\n\e[0m"
     else
         echo -e "\e[1;31mFAILURE\n\e[0m"
+        echo -e "\e[0;31mNote : I volontary don't print the diff for 1000x1000 map\n\e[0m"
     fi
     rm test_bash_log.txt
 
@@ -476,6 +480,63 @@ function test () {
         echo -e "\e[1;32mSUCESS\n\e[0m"
     else
         echo -e "\e[1;31mFAILURE\n\e[0m"
+    fi
+    rm test_bash_log.txt
+
+    echo -e "\n\e[1;33m-----[Generating map (solved + optimisation)]-----\n\e[0m"; sleep 0.1
+
+    echo -e "\e[1;37mtest n°1 (Generating 100x100 map (5s max)) :"; sleep 0.1
+    timeout 5s ./setting_up 100 ....o.o... > test_bash_log.txt
+    sdiff -s test_bash_log.txt bash_test/generating_100x100_map.txt
+    if [ $? -eq 0 ]; then
+        echo -e "\e[1;32mSUCESS\n\e[0m"
+    else
+        echo -e "\e[1;31mFAILURE\n\e[0m"
+        echo -e "\e[0;31mTrouble with generating time ?\nNote : don't use your putchar in the loop of your putstr\n\e[0m"
+    fi
+    rm test_bash_log.txt
+
+    echo -e "\e[1;37mtest n°2 (Generating 500x500 map (10s max)) :"; sleep 0.1
+    timeout 10s ./setting_up 500 ....o.o...o.........o.o > test_bash_log.txt
+    sdiff -s test_bash_log.txt bash_test/generating_500x500_map.txt
+    if [ $? -eq 0 ]; then
+        echo -e "\e[1;32mSUCESS\n\e[0m"
+    else
+        echo -e "\e[1;31mFAILURE\n\e[0m"
+        echo -e "\e[0;31mTrouble with generating time ?\nNote : don't use your putchar in the loop of your putstr\n\e[0m"
+    fi
+    rm test_bash_log.txt
+
+    echo -e "\e[1;37mtest n°3 (Generating 1000x1000 map (15s max)) :";
+    timeout 15s ./setting_up 1000 ....o.o...o.........o.o..o > test_bash_log.txt
+    sdiff -s test_bash_log.txt bash_test/generating_1000x1000_map.txt > /dev/null
+    if [ $? -eq 0 ]; then
+        echo -e "\e[1;32mSUCESS\n\e[0m"
+    else
+        echo -e "\e[1;31mFAILURE\n\e[0m"
+        echo -e "\e[0;31mTrouble with generating time ?\nNote : don't use your putchar in the loop of your putstr\n\e[0m"
+    fi
+    rm test_bash_log.txt
+
+    echo -e "\e[1;37mtest n°4 (Generating 5000x5000 map (25s max)) :";
+    timeout 25s ./setting_up 5000 ....o.o...o.........o.o...o....o > test_bash_log.txt
+    sdiff -s test_bash_log.txt bash_test/generating_5000x5000_map.txt > /dev/null
+    if [ $? -eq 0 ]; then
+        echo -e "\e[1;32mSUCESS\n\e[0m"
+    else
+        echo -e "\e[1;31mFAILURE\n\e[0m"
+        echo -e "\e[0;31mTrouble with generating time ?\nNote : don't use your putchar in the loop of your putstr\n(PS : Your so bad i have 5s on this test)n\e[0m"
+    fi
+    rm test_bash_log.txt
+
+    echo -e "\e[1;37mtest n°5 (Generating 10000x10000 map (40s max)) :";
+    timeout 40s ./setting_up 10000 ....o.o...o.........o.o..o..o........ > test_bash_log.txt
+    sdiff -s test_bash_log.txt bash_test/generating_10000x10000_map.txt > /dev/null
+    if [ $? -eq 0 ]; then
+        echo -e "\e[1;32mSUCESS\n\e[0m"
+    else
+        echo -e "\e[1;31mFAILURE\n\e[0m"
+        echo -e "\e[0;31mTrouble with generating time ?\nNote : don't use your putchar in the loop of your putstr\n(PS : Your so bad i have 10s on this test)\n\e[0m"
     fi
     rm test_bash_log.txt
 }
