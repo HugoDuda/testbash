4ebe43921b91c4be8015760ed37146b3c514c8ec
- <hugo.duda@protonmail.com>
2023-12-30 17:45:27 +0100

    [ADD] Test bash my_str_to_word_array

---
+++ my_str_to_word_array/my.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** my.h
+** File description:
+** my.h
+*/
+
+#pragma once
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char **my_str_to_word_array(char const *str, char sp);
